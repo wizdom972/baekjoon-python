973f3fe6905d077a488d72c2a746b8779414ff24
Lee Jihye <jihae0584@gmail.com>
2024-02-14 15:10:30 +0900

    [DoIt] 03 연속된 자연수의 합 구하기
    백준 2018번 문제

---
+++ Do it 알고리즘 코딩 테스트 파이썬 편/03 자료구조/2018.py
@@ -0,0 +1,23 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+
+count = 1
+start_index = 1
+end_index = 1
+sum = 1
+
+while end_index != n:
+    if sum == n:
+        count += 1
+        end_index += 1
+        sum += end_index
+    elif sum > n:
+        sum -= start_index
+        start_index += 1
+    else:   # sum < n
+        end_index += 1
+        sum += end_index
+
+print(count)
